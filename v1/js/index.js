af33609d0e01bd6a1288a633fa343b85a9c0fd66
薛源 <xueyuan@liangteam.com>
2020-01-14 10:14:19 +0800

    数据代理

--- v1/js/index.js
+++ v1/js/index.js
@@ -1,7 +1,7 @@
 function SelfVue (data, el, exp) {
     var self = this;
     this.data = data;
-
+    //下边的功能是 将 数据都代理到 this 上 this.xxx=this.data[xxx]
     Object.keys(data).forEach(function(key) {
         self.proxyKeys(key);
     });
